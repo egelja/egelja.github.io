ff15b2e3838ee38c61d0626fa4c4b7848ca9f7ff
Nino Maruszewski <nino.maruszewski@gmail.com>
2023-08-28 12:46:42 -0500

    fix: don't cut off zoom

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,7 +1,11 @@
 // Initialize medium zoom.
 $(document).ready(function() {
-  medium_zoom = mediumZoom('[data-zoomable]', {
-    background: getComputedStyle(document.documentElement)
-        .getPropertyValue('--global-bg-color') + 'ee',  // + 'ee' for trasparency.
-  })
+    medium_zoom = mediumZoom('[data-zoomable]', {
+        background: getComputedStyle(document.documentElement)
+            .getPropertyValue('--global-bg-color') + 'ee',  // + 'ee' for trasparency.
+        container: {
+            top: 64,
+            bottom: 42,
+        },
+    })
 });
