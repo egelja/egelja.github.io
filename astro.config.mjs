e862699908a2b80785d7e8f19fc7f1d82f8a9d6e
Nino Maruszewski <nino.maruszewski@gmail.com>
2025-07-07 19:53:15 -0500

    add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,15 @@ import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://marusz.com",
   base: "/",
   trailingSlash: "always",
 
-  integrations: [sitemap(), preact(), mdx()],
+  integrations: [sitemap(), preact(), mdx(), icon()],
 
   vite: {
     plugins: [tailwindcss()],
